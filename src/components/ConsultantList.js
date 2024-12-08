68a63c4eb6b0bf5c1aaab142dc1f9f5d8eb6e433
e2302260 <e2302260@edu.vamk.fi>
2024-12-08 20:46:04 +0200

    Lisätty hakupalkki konsulttilistan.

--- src/components/ConsultantList.js
+++ src/components/ConsultantList.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import consultants from '../data/consultants';
 import './ConsultantList.css';
 
 const ConsultantList = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredConsultants = consultants.filter(consultant =>
+    consultant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="consultant-list">
       <h1>Konsulttilista</h1>
+      <input
+        type="text"
+        placeholder="Hae nimellä..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <ul>
-        {consultants.map(consultant => (
+        {filteredConsultants.map(consultant => (
           <li key={consultant.id}>
             <h2>{consultant.name}</h2>
             <p>{consultant.technologies.join(", ")}</p>
